2bdaa6dc0a088c7bc413fdf960bf205de946334a
Anders <anders.bjoerk.unf@gmail.com>
2025-12-28 19:17:11 +0100

    [ADD] added main.c and the cmake to build it

---
+++ main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author Anders Bjørk (anders.bjoerk.unf@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-12-28
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+
+int main(void)
+{
+    while(1) {};
+    return 0;
+}
+
