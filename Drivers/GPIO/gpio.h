630cfacf0a50b923eef7df9961bc6d96040ff8e5
Anders <anders.bjoerk.unf@gmail.com>
2026-01-15 20:38:30 +0100

    [FIX] updated the GPIO header

--- Drivers/GPIO/gpio.h
+++ Drivers/GPIO/gpio.h
@@ -10,5 +10,29 @@
  */
 #pragma once
 
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
 
-void inti_gpio(void);
+typedef enum PinMode_etag{
+    OUTPUT,
+    INPUT
+}PinMode_e;
+
+typedef enum PinState_etag{
+    LOW = 0,
+    HIGH = 1
+}PinState_e;
+
+typedef enum GPIO_IRQ_MODE_etag{
+    FALLING_EDGE,
+    RISING_EDGE,
+    CHANGE
+}GPIO_IRQ_MODE_t;
+
+void    DRV_GPIO_Init(uint16_t pin, PinMode_e dir);
+void    DRV_GPIO_Set(uint16_t pin, PinState_e state);
+bool    DRV_GPIO_Get(uint16_t pin, PinState_e *state);
+void    DRV_GPIO_Toggle(uint16_t pin);
+
+void    DRV_GPIO_SetupIRQ(uint16_t pin, GPIO_IRQ_MODE_t mode);
