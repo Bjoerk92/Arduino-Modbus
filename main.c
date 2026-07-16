fe84ad84e36d1ce57d95a4548b66af9a18ffd73b
Anders <anders.bjoerk.unf@gmail.com>
2026-07-16 21:19:38 +0200

    [FIX] changes to the components

--- main.c
+++ main.c
@@ -9,26 +9,10 @@
  * 
  */
 #include "Atmega.h"
-#include "Timers.h"
 
-
-static void TimerOverflowCallback(void) {
-    // This function will be called when the timer overflows
-    // You can toggle an LED, send a message, or perform any other action here
-}
 
 static void InitDrv(void) {
     
-    Timer_config_t Timer_cfg = {
-        .timer_id = TIMER_ID_1,
-        .wave_gen_mode = WAVE_GEN_MODE_NORMAL,
-        .clock_select = CLOCK_SELECT_CLK_64,
-        .timer_irq_mode = TIMER_IRQ_MODE_OVERFLOW,
-        .timer_irq_callback = TimerOverflowCallback,
-    };
-
-    Timers_Init(&Timer_cfg);
-
 };
 
 
